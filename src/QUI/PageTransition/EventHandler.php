dc8a47f04d5b183e9821a4e2207186a41dc49cb6
Henning Leutz <leutz@pcsg.de>
2016-10-20 07:44:14 +0200

    refactor: first version

---
+++ src/QUI/PageTransition/EventHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file contains QUI\PageTransition\EventHandler
+ */
+namespace QUI\PageTransition;
+
+use QUI;
+
+/**
+ * Class EventHandler
+ *
+ * @package QUI\PageTransition
+ */
+class EventHandler
+{
+    public static function onTemplateGetHeader(QUI\Template $Template)
+    {
+echo 1;
+    }
+}
