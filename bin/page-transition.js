54811b5659c9001c5ea1bd23245d1e77842c497b
michael.daniel <michael@pcsg.de>
2018-11-06 11:23:12 +0100

    fix: Rewritten in vanilla js by @henbug (quiqqer/page-transition#5).

--- bin/page-transition.js
+++ bin/page-transition.js
@@ -1,12 +1,53 @@
 (function () {
     'use strict';
 
+    /**
+     * Get the closest matching element up the DOM tree.
+     * Helper for no conflict mode
+     *
+     * @param  {Element} elem     Starting element
+     * @param  {String}  selector Selector to match against
+     * @return {Boolean|Element}  Returns null if not match found
+     */
+    var getParent = function (elem, selector) {
+        // Element.matches() polyfill
+        if (!Element.prototype.matches) {
+            Element.prototype.matches =
+                Element.prototype.matchesSelector ||
+                Element.prototype.mozMatchesSelector ||
+                Element.prototype.msMatchesSelector ||
+                Element.prototype.oMatchesSelector ||
+                Element.prototype.webkitMatchesSelector ||
+                function (s) {
+                    var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                        i       = matches.length;
+                    while (--i >= 0 && matches.item(i) !== this) {
+                    }
+                    return i > -1;
+                };
+        }
+
+        // Get closest match
+        for (; elem && elem !== document; elem = elem.parentNode) {
+            if (elem.matches(selector)) {
+                return elem;
+            }
+        }
+
+        return null;
+    };
+
+    /**
+     * execute when dom is ready
+     */
     var ready = function () {
         function addEvent(Elm, event, func) {
             if (typeof Elm.addEventListener !== "undefined") {
                 Elm.addEventListener(event, func, false);
-            } else if (typeof Elm.attachEvent !== "undefined") {
-                Elm.attachEvent("on" + event, func);
+            } else {
+                if (typeof Elm.attachEvent !== "undefined") {
+                    Elm.attachEvent("on" + event, func);
+                }
             }
         }
 
@@ -18,7 +59,7 @@
                     return;
                 }
 
-                Container[0].style.zIndex  = -1;
+                Container[0].style.zIndex = -1;
                 Container[0].style.opacity = 0;
             };
 
@@ -49,8 +90,13 @@
         });
 
         // pace bug fix mailto link - quiqqer/page-transition#4
-        document.getElements('a').addEvent('click', function (event) {
+        var links = document.querySelectorAll('a');
+        var linkClick = function (event) {
             var Target = event.target;
+
+            if (Target.nodeName !== 'A') {
+                Target = getParent(Target, 'A');
+            }
 
             if (Target.href.toString().indexOf('mailto:') !== -1) {
                 window.Pace.restart();
@@ -58,11 +104,15 @@
                 return;
             }
 
-            if (Target.get('target').toString().indexOf('_blank') !== -1) {
+            if (Target.target.toString().indexOf('_blank') !== -1) {
                 window.Pace.restart();
                 window.Pace.stop();
             }
-        });
+        };
+
+        for (var i = 0, len = links.length; i < len; i++) {
+            links[i].addEventListener('click', linkClick);
+        }
 
         // show page
         window.Pace.on('done', function () {
@@ -72,7 +122,7 @@
                 return;
             }
 
-            document.body.addClass('quiqqer-page-transition-loaded');
+            document.body.classList.add('quiqqer-page-transition-loaded');
 
             if (typeof window.QUIQQER_PAGE_TRANSITION === 'undefined') {
                 window.QUIQQER_PAGE_TRANSITION = '';
@@ -81,7 +131,7 @@
             Container = Container[0];
 
             if (window.QUIQQER_PAGE_TRANSITION === '') {
-                Container.style.zIndex  = -1;
+                Container.style.zIndex = -1;
                 Container.style.opacity = 0;
                 return;
             }
@@ -102,7 +152,7 @@
             Container.addClass(window.QUIQQER_PAGE_TRANSITION + '-hide');
 
             setTimeout(function () {
-                Container.style.zIndex  = -1;
+                Container.style.zIndex = -1;
                 Container.style.opacity = 0;
             }, 1000);
         });
